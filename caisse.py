358b04522dd161cededb1f1b9b8298c2527433c8
flannois <florent.lannois@gmail.com>
2024-10-25 00:31:40 +0200

    Variable change mais pas d'actualisation sur la fenetre

--- caisse.py
+++ caisse.py
@@ -1,5 +1,7 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.button import Button
+
 
 # Gestion d'import des différentes fenêtres
 class Fenetre_Principale(Screen):
@@ -14,19 +16,29 @@ class Fenetre_Produits(Screen):
 class Fenetre_Categories(Screen):
     pass
 
+class Fenetre_Suppression(Screen):
+    pass
 
 
 # Gestionnaire d'écran
 class CaisseApp(App):
     def build(self):
         # Création d'un ScreenManager pour gérer les transitions
-        sm = ScreenManager()
-        sm.add_widget(Fenetre_Principale(name='Fenetre_Principale'))
-        sm.add_widget(Fenetre_Configuration(name='Fenetre_Configuration'))
-        sm.add_widget(Fenetre_Categories(name='Fenetre_Categories'))
-        sm.add_widget(Fenetre_Produits(name='Fenetre_Produits'))
+        self.sm = ScreenManager()
+        self.sm.add_widget(Fenetre_Principale(name='Fenetre_Principale'))
+        self.sm.add_widget(Fenetre_Configuration(name='Fenetre_Configuration'))
+        self.sm.add_widget(Fenetre_Categories(name='Fenetre_Categories'))
+        self.sm.add_widget(Fenetre_Produits(name='Fenetre_Produits'))
+        self.sm.add_widget(Fenetre_Suppression(name='Fenetre_Suppression'))
         
-        return sm
+        return self.sm
+
+    def updateCategories(self):
+        self.fen = Fenetre_Principale()
+        print(self.fen.ids.label_test.text)
+
+        self.fen.ids.label_test.text = "OKOKOK"
+        print(self.fen.ids.label_test.text)
 
     def appui_bouton(self, btn):
         print(btn.text)
